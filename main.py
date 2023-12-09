761838987ebc1e13baf6700196a4acd0156ddc33
kirilllllllllllllllllllllllllllll <150334314+kirilllllllllllllllllllllllllllll@users.noreply.github.com>
2023-12-09 18:46:36 +0700

    Add files via upload
    commit1: score

--- main.py
+++ main.py
@@ -1,140 +1,5 @@
-import os
-import sys
-
 import pygame
-import random
-from math import cos, pi, sin
-
-# Изображение не получится загрузить
-# без предварительной инициализации pygame
-pygame.init()
-size = width, height = 500, 500
-screen = pygame.display.set_mode(size)
-
-
-def load_image(name, colorkey=None):
-    fullname = os.path.join('data', name)
-    if not os.path.isfile(fullname):
-        print(f"Файл с изображением '{fullname}' не найден")
-        sys.exit()
-    image = pygame.image.load(fullname)
-    if colorkey is not None:
-        image = image.convert()
-        if colorkey == -1:
-            colorkey = image.get_at((0, 0))
-        image.set_colorkey(colorkey)
-    else:
-        image = image.convert_alpha()
-    return image
-
-
-class Bomb(pygame.sprite.Sprite):
-    image = load_image("bomb.png")
-    image_boom = load_image("boom.png")
-
-    def __init__(self, group, corner):
-        # НЕОБХОДИМО вызвать конструктор родительского класса Sprite.
-        # Это очень важно !!!
-        super().__init__(group)
-        self.image = Bomb.image
-        self.rect = self.image.get_rect()
-        self.corner = corner
-        self.center_x = -400
-        self.center_y = 400
-        self.side = 300
-        self.rect.x = self.center_x + int(self.side * cos(self.corner * pi / 180)) - 25
-        self.rect.y = self.center_y + int(self.side * sin(self.corner * pi / 180)) - 25
-        self.clock1 = pygame.time.Clock()
-        self.clock2 = pygame.time.Clock()
-        self.v_main = 10
-        self.v_x = 15
-
-    def update(self):
-        global death
-        self.image = Bomb.image
-        if start:
-            self.corner += self.v_main * self.clock1.tick() / 100
-            self.corner %= 360
-            self.center_x += self.v_x * self.clock2.tick() / 100
-            if self.center_x >= 800 + self.side + 50:
-                self.side = random.randint(75, 320)
-                self.center_x = -self.side - 40
-                self.v_main = random.randint(5, 15)
-                self.center_y = random.randint(self.side, 800 - self.side)
-                self.clock1 = pygame.time.Clock()
-                self.clock2 = pygame.time.Clock()
-            self.rect.x = self.center_x + int(self.side * cos(self.corner * pi / 180)) - 25
-            self.rect.y = self.center_y + int(self.side * sin(self.corner * pi / 180)) - 25
-            if self.rect.colliderect(sprite.rect):
-                screen.fill((255, 0, 0))
-                self.image = self.image_boom
-                death = True
-
-
-class Creature(pygame.sprite.Sprite):
-    image = load_image("creature.png", -1)
-
-    def __init__(self, group):
-        super().__init__(group)
-        self.image = Creature.image
-        self.rect = self.image.get_rect()
-        self.rect.x = 400
-        self.rect.y = 400
-        self.go_up = False
-        self.go_down = False
-        self.go_left = False
-        self.go_right = False
-        self.clock_x = pygame.time.Clock()
-        self.clock_y = pygame.time.Clock()
-        self.v_x = 0
-        self.v_y = 0
-
-    def update(self, *args):
-        if args:
-            if args[0].type == pygame.KEYDOWN and args[0].key == pygame.K_DOWN and not self.go_down:
-                # print('down')
-                self.v_y = 50
-                self.go_down = True
-            elif args[0].type == pygame.KEYUP and args[0].key == pygame.K_DOWN and self.go_down:
-                self.v_y = 0
-                self.go_down = False
-
-            if args[0].type == pygame.KEYDOWN and args[0].key == pygame.K_UP and not self.go_up:
-                self.v_y = -70
-                self.go_up = True
-            elif args[0].type == pygame.KEYUP and args[0].key == pygame.K_UP and self.go_up:
-                self.v_y = 0
-                self.go_up = False
-
-            if args[0].type == pygame.KEYDOWN and args[0].key == pygame.K_LEFT and not self.go_left:
-                self.v_x = -70
-                self.go_left = True
-            elif args[0].type == pygame.KEYUP and args[0].key == pygame.K_LEFT and self.go_left:
-                self.v_x = 0
-                self.go_left = False
-
-            if args[0].type == pygame.KEYDOWN and args[0].key == pygame.K_RIGHT and not self.go_right:
-                self.v_x = 50
-                self.go_right = True
-            elif args[0].type == pygame.KEYUP and args[0].key == pygame.K_RIGHT and self.go_right:
-                self.v_x = 0
-                self.go_right = False
-            self.rect.y += self.v_y * self.clock_y.tick() / 1000
-            self.rect.x += self.v_x * self.clock_x.tick() / 1000
-            # print('================', self.rect.x, self.rect.y, self.v_x, self.v_y)
-        else:
-            self.rect.y += self.v_y * self.clock_y.tick() / 100
-            self.rect.x += self.v_x * self.clock_x.tick() / 100
-            # print(self.rect.x, self.rect.y, self.v_x, self.v_y)
-        if self.rect.x < 0:
-            self.rect.x = 0
-        if self.rect.x > 720:
-            self.rect.x = 720
-        if self.rect.y < 0:
-            self.rect.y = 0
-        if self.rect.y > 700:
-            self.rect.y = 700
-
+from math import cos, sin, pi
 
 if __name__ == '__main__':
     pygame.init()
@@ -147,61 +12,73 @@ if __name__ == '__main__':
     # x = 10
     move = False
     a = 70
-    all_sprites = pygame.sprite.Group()
-    main_sprite = pygame.sprite.Group()
-    sprite = Creature(main_sprite)
-
-    corners = (30, 150, 270)
-    for i in range(3):
-        Bomb(all_sprites, corners[i])
-
-
-    drawing = False  # режим рисования выключен
+    pygame.draw.circle(screen, (255, 255, 255), center, 10, width=0)
+    pygame.draw.polygon(screen, (255, 255, 255),
+                        (center, (400 + int(a * cos(15 * pi / 180)), 400 + int(a * sin(15 * pi / 180))),
+                         (400 + int(a * cos(45 * pi / 180)), 400 + int(a * sin(45 * pi / 180)))),
+                        width=0)
+    pygame.draw.polygon(screen, (255, 255, 255),
+                        (center, (400 + int(a * cos(255 * pi / 180)), 400 + int(a * sin(255 * pi / 180))),
+                         (400 + int(a * cos(285 * pi / 180)), 400 + int(a * sin(285 * pi / 180)))),
+                        width=0)
+    pygame.draw.polygon(screen, (255, 255, 255),
+                        (center, (400 + int(a * cos(135 * pi / 180)), 400 + int(a * sin(135 * pi / 180))),
+                         (400 + int(a * cos(165 * pi / 180)), 400 + int(a * sin(165 * pi / 180)))),
+                        width=0)
+    corner1 = 15
+    corner2 = 45
+    corner3 = 255
+    corner4 = 285
+    corner5 = 135
+    corner6 = 165
     start = False
-    death = False
-    all_sprites.draw(screen)
-    screen.fill((0, 0, 255))
+    v = 50
+    clock1 = pygame.time.Clock()
+    clock2 = pygame.time.Clock()
+    clock3 = pygame.time.Clock()
+    clock4 = pygame.time.Clock()
+    clock5 = pygame.time.Clock()
+    clock6 = pygame.time.Clock()
+    data = []
+    drawing = False  # режим рисования выключен
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE and start:
-                start = False
-            elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE and not start:
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 start = True
+                v += 50
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:
+                start = True
+                v -= 50
+        if start:
+            screen.fill((0, 0, 0))
+            pygame.draw.circle(screen, (255, 255, 255), center, 10, width=0)
+            corner1 += v * clock1.tick() / 100
+            corner2 += v * clock2.tick() / 100
+            corner3 += v * clock3.tick() / 100
+            corner4 += v * clock4.tick() / 100
+            corner5 += v * clock5.tick() / 100
+            corner6 += v * clock6.tick() / 100
+            corner1 = corner1 % 360
+            corner2 = corner2 % 360
+            corner3 = corner3 % 360
+            corner4 = corner4 % 360
+            corner5 = corner5 % 360
+            corner6 = corner6 % 360
+            pygame.draw.polygon(screen, (255, 255, 255),
+                                (center, (400 + int(a * cos(corner1 * pi / 180)), 400 + int(a * sin(corner1 * pi / 180))),
+                                 (400 + int(a * cos(corner2 * pi / 180)), 400 + int(a * sin(corner2 * pi / 180)))),
+                                width=0)
+            pygame.draw.polygon(screen, (255, 255, 255),
+                                (center, (400 + int(a * cos(corner3 * pi / 180)), 400 + int(a * sin(corner3 * pi / 180))),
+                                 (400 + int(a * cos(corner4 * pi / 180)), 400 + int(a * sin(corner4 * pi / 180)))),
+                                width=0)
+            pygame.draw.polygon(screen, (255, 255, 255),
+                                (center, (400 + int(a * cos(corner5 * pi / 180)), 400 + int(a * sin(corner5 * pi / 180))),
+                                 (400 + int(a * cos(corner6 * pi / 180)), 400 + int(a * sin(corner6 * pi / 180)))),
+                                width=0)
+            # рисуем на экране сохранённое на втором холсте
 
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_q and death:
-                death = False
-                all_sprites = pygame.sprite.Group()
-                corners = (30, 150, 270)
-                for i in range(3):
-                    Bomb(all_sprites, corners[i])
-
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN and not sprite.go_down:
-                main_sprite.update(event)
-            elif event.type == pygame.KEYUP and event.key == pygame.K_DOWN and sprite.go_down:
-                main_sprite.update(event)
-
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_UP and not sprite.go_up:
-                main_sprite.update(event)
-            elif event.type == pygame.KEYUP and event.key == pygame.K_UP and sprite.go_up:
-                main_sprite.update(event)
-
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT and not sprite.go_left:
-                main_sprite.update(event)
-            elif event.type == pygame.KEYUP and event.key == pygame.K_LEFT and sprite.go_left:
-                main_sprite.update(event)
-
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT and not sprite.go_right:
-                main_sprite.update(event)
-            elif event.type == pygame.KEYUP and event.key == pygame.K_RIGHT and sprite.go_right:
-                main_sprite.update(event)
-
-        if not death:
-            screen.fill((0, 0, 255))
-            all_sprites.update()
-            all_sprites.draw(screen)
-            main_sprite.update()
-            main_sprite.draw(screen)
-            pygame.display.flip()
+        pygame.display.flip()
     pygame.quit()
