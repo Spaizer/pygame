d4502bb409cb8ce4e8e9be8d231f3e5dcf1ae16a
kirilllllllllllllllllllllllllllll <150334314+kirilllllllllllllllllllllllllllll@users.noreply.github.com>
2023-12-10 14:23:35 +0700

    Add files via upload
    commit3: hello_level и несколько персонажей

--- main.py
+++ main.py
@@ -94,11 +94,30 @@ class Bomb(pygame.sprite.Sprite):
 
 
 class Creature(pygame.sprite.Sprite):
-    image = load_image("creature.png", -1)
+    image1_go_right = pygame.transform.scale(load_image("go_right.jpg", -1), (166, 80))
+    image1_go_down = pygame.transform.scale(load_image("go_down.jpg", -1), (72, 80))
+    image1_go_up = pygame.transform.scale(load_image("go_up.jpg", -1), (80, 80))
+    image1_go_left = pygame.transform.flip(image1_go_right, True, False)
 
-    def __init__(self, group):
+    image2_go_right = pygame.transform.scale(load_image("go_right2.png", -1), (166, 80))
+    image2_go_down = pygame.transform.scale(load_image("go_down2.png", -1), (72, 80))
+    image2_go_up = pygame.transform.scale(load_image("go_up2.png", -1), (80, 80))
+    image2_go_left = pygame.transform.flip(image2_go_right, True, False)
+
+    image3_go_right = pygame.transform.scale(load_image("go_right3.png", -1), (166, 80))
+    image3_go_down = pygame.transform.scale(load_image("go_down3.png", -1), (72, 80))
+    image3_go_up = pygame.transform.scale(load_image("go_up3.png", -1), (80, 80))
+    image3_go_left = pygame.transform.flip(image3_go_right, True, False)
+
+    def __init__(self, group, num):
         super().__init__(group)
-        self.image = Creature.image
+        self.num = num
+        if num == 0:
+            self.image = Creature.image1_go_right
+        elif num == 1:
+            self.image = Creature.image2_go_right
+        else:
+            self.image = Creature.image3_go_right
         self.rect = self.image.get_rect()
         self.rect.x = 400
         self.rect.y = 400
@@ -115,6 +134,12 @@ class Creature(pygame.sprite.Sprite):
         if args:
             if args[0].type == pygame.KEYDOWN and args[0].key == pygame.K_DOWN and not self.go_down:
                 # print('down')
+                if self.num == 0:
+                    self.image = Creature.image1_go_down
+                elif num == 1:
+                    self.image = Creature.image2_go_down
+                else:
+                    self.image = Creature.image3_go_down
                 self.v_y = 50
                 self.go_down = True
             elif args[0].type == pygame.KEYUP and args[0].key == pygame.K_DOWN and self.go_down:
@@ -122,6 +147,12 @@ class Creature(pygame.sprite.Sprite):
                 self.go_down = False
 
             if args[0].type == pygame.KEYDOWN and args[0].key == pygame.K_UP and not self.go_up:
+                if self.num == 0:
+                    self.image = Creature.image1_go_up
+                elif num == 1:
+                    self.image = Creature.image2_go_up
+                else:
+                    self.image = Creature.image3_go_up
                 self.v_y = -70
                 self.go_up = True
             elif args[0].type == pygame.KEYUP and args[0].key == pygame.K_UP and self.go_up:
@@ -129,6 +160,12 @@ class Creature(pygame.sprite.Sprite):
                 self.go_up = False
 
             if args[0].type == pygame.KEYDOWN and args[0].key == pygame.K_LEFT and not self.go_left:
+                if self.num == 0:
+                    self.image = Creature.image1_go_left
+                elif num == 1:
+                    self.image = Creature.image2_go_left
+                else:
+                    self.image = Creature.image3_go_left
                 self.v_x = -70
                 self.go_left = True
             elif args[0].type == pygame.KEYUP and args[0].key == pygame.K_LEFT and self.go_left:
@@ -136,6 +173,12 @@ class Creature(pygame.sprite.Sprite):
                 self.go_left = False
 
             if args[0].type == pygame.KEYDOWN and args[0].key == pygame.K_RIGHT and not self.go_right:
+                if self.num == 0:
+                    self.image = Creature.image1_go_right
+                elif num == 1:
+                    self.image = Creature.image2_go_right
+                else:
+                    self.image = Creature.image3_go_right
                 self.v_x = 50
                 self.go_right = True
             elif args[0].type == pygame.KEYUP and args[0].key == pygame.K_RIGHT and self.go_right:
@@ -150,46 +193,39 @@ class Creature(pygame.sprite.Sprite):
             # print(self.rect.x, self.rect.y, self.v_x, self.v_y)
         if self.rect.x < 0:
             self.rect.x = 0
-        if self.rect.x > 720:
-            self.rect.x = 720
+        if self.rect.x > 634:
+            self.rect.x = 634
         if self.rect.y < 0:
             self.rect.y = 0
-        if self.rect.y > 700:
-            self.rect.y = 700
+        if self.rect.y > 720:
+            self.rect.y = 720
 
 
-if __name__ == '__main__':
-    pygame.init()
-    size = width, height = 800, 800
-    screen = pygame.display.set_mode(size)
-    running = True
-    # drawing = False
+def level_with_bombs():
+    global start, death, score, screen
+    running_level_with_bombs = True
     center = (400, 400)
-    # x = 10
-    move = False
-    score = 0
     all_sprites = pygame.sprite.Group()
-    main_sprite = pygame.sprite.Group()
-    sprite = Creature(main_sprite)
+
     score_f(screen)
 
     corners = (30, 150, 270)
     for i in range(3):
         Bomb(all_sprites, corners[i])
 
-
-    start = False
-    death = False
+    stop_all = False
     all_sprites.draw(screen)
     screen.fill((0, 0, 255))
-    while running:
+    start = True
+    while running_level_with_bombs:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE and start:
-                start = False
-            elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE and not start:
-                start = True
+                running_level_with_bombs = False
+                stop_all = True
+            # if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE and start:
+            #     start = False
+            # elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE and not start:
+            #     start = True
 
             if event.type == pygame.KEYDOWN and event.key == pygame.K_q and death:
                 death = False
@@ -229,4 +265,72 @@ if __name__ == '__main__':
             pygame.display.flip()
         else:
             score_f(screen, True)
-    pygame.quit()
+    if stop_all:
+        pygame.quit()
+        return False
+    return True
+
+
+if __name__ == '__main__':
+    score = 0
+    pygame.init()
+    size = width, height = 800, 800
+    screen = pygame.display.set_mode(size)
+
+    # приветствие
+    screen.fill('#5C6BC0')
+    running_start = True
+    image1_go_right = pygame.transform.scale(load_image("go_right.jpg", -1), (166, 80))
+    image1_go_down = pygame.transform.scale(load_image("go_down.jpg", -1), (72, 80))
+    image1_go_up = pygame.transform.scale(load_image("go_up.jpg", -1), (80, 80))
+    image1_go_left = pygame.transform.flip(image1_go_right, True, False)
+
+    image2_go_right = pygame.transform.scale(load_image("go_right2.png", -1), (166, 80))
+    image2_go_down = pygame.transform.scale(load_image("go_down2.png", -1), (72, 80))
+    image2_go_up = pygame.transform.scale(load_image("go_up2.png", -1), (80, 80))
+    image2_go_left = pygame.transform.flip(image2_go_right, True, False)
+
+    image3_go_right = pygame.transform.scale(load_image("go_right3.png", -1), (166, 80))
+    image3_go_down = pygame.transform.scale(load_image("go_down3.png", -1), (72, 80))
+    image3_go_up = pygame.transform.scale(load_image("go_up3.png", -1), (80, 80))
+    image3_go_left = pygame.transform.flip(image3_go_right, True, False)
+
+    running_hello_level = True
+    screen.blit(image1_go_right, (75, 125))
+    screen.blit(image2_go_right, (316, 125))
+    screen.blit(image3_go_right, (557, 125))
+    pygame.draw.rect(screen, (255, 255, 255), (75, 330, 166, 100), width=2)
+    pygame.draw.rect(screen, (255, 255, 255), (316, 330, 166, 100), width=2)
+    pygame.draw.rect(screen, (255, 255, 255), (557, 330, 166, 100), width=2)
+    stop_all = False
+    num = None
+    while running_hello_level:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running_hello_level = False
+                stop_all = True
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                if event.pos[1] >= 330 and event.pos[1] <= 430:
+                    if event.pos[0] >= 75 and event.pos[0] <= 241:
+                        num = 0
+                    elif event.pos[0] >= 316 and event.pos[0] <= 482:
+                        num = 1
+                    elif event.pos[0] >= 557 and event.pos[0] <= 723:
+                        num = 2
+        if num is not None:
+            break
+
+        pygame.display.flip()
+
+    # если не закрыли окно
+    if not stop_all:
+        main_sprite = pygame.sprite.Group()
+        sprite = Creature(main_sprite, num)
+
+        # следующий уровень
+        start = False
+        death = False
+        if level_with_bombs():  # опять проверяем, закрыто ли окно
+            # следующий уровень
+            pass
+
