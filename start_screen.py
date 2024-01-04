d787ef7049c52d810d0de0163fd1c755fd5e9738
kirilllllllllllllllllllllllllllll <150334314+kirilllllllllllllllllllllllllllll@users.noreply.github.com>
2024-01-04 11:23:45 +0700

    Add files via upload
    commit: заставки

---
+++ start_screen.py
@@ -0,0 +1,30 @@
+from terminate import terminate
+import pygame
+from load_image import load_image
+FPS = 50
+
+
+def start_screen(screen, intro_text, name, scale):
+    fon = pygame.transform.scale(load_image(name), scale)
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    clock = pygame.time.Clock()
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('black'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return  # начинаем игру
+        pygame.display.flip()
+        clock.tick(FPS)
