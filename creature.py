b29ade9105dfb67a87ec44c04221cfe97091cf0d
kirilllllllllllllllllllllllllllll <150334314+kirilllllllllllllllllllllllllllll@users.noreply.github.com>
2024-01-08 12:52:19 +0700

    Add files via upload
    commit: настроена скорость передвижения

--- creature.py
+++ creature.py
@@ -3,21 +3,13 @@ from load_image import load_image
 
 
 class Creature(pygame.sprite.Sprite):
-    def __init__(self, group, num):
+    def __init__(self, group, num, level, slow=False):
         image1_go_right = pygame.transform.scale(load_image("go_right.jpg", -1), (125, 60))
-        image1_go_down = pygame.transform.scale(load_image("go_down.jpg", -1), (54, 60))
-        image1_go_up = pygame.transform.scale(load_image("go_up.jpg", -1), (60, 60))
-        image1_go_left = pygame.transform.flip(image1_go_right, True, False)
 
         image2_go_right = pygame.transform.scale(load_image("go_right2.png", -1), (125, 60))
-        image2_go_down = pygame.transform.scale(load_image("go_down2.png", -1), (54, 60))
-        image2_go_up = pygame.transform.scale(load_image("go_up2.png", -1), (60, 80))
-        image2_go_left = pygame.transform.flip(image2_go_right, True, False)
 
         image3_go_right = pygame.transform.scale(load_image("go_right3.png", -1), (125, 60))
-        image3_go_down = pygame.transform.scale(load_image("go_down3.png", -1), (54, 60))
-        image3_go_up = pygame.transform.scale(load_image("go_up3.png", -1), (60, 80))
-        image3_go_left = pygame.transform.flip(image3_go_right, True, False)
+
         super().__init__(group)
         self.num = num
         if num == 0:
@@ -40,6 +32,9 @@ class Creature(pygame.sprite.Sprite):
         self.v_y = 0
         self.death = False
         self.score = 0
+        self.level = level
+
+        self.slow = slow
 
     def update(self, *args):
         if args:
@@ -59,26 +54,34 @@ class Creature(pygame.sprite.Sprite):
             image3_go_left = pygame.transform.flip(image3_go_right, True, False)
             if args[0].type == pygame.KEYDOWN and args[0].key == pygame.K_DOWN and not self.go_down:
                 # print('down')
-                if self.num == 0:
-                    self.image = image1_go_down
-                elif self.num == 1:
-                    self.image = image2_go_down
+                if self.level != 4:
+                    if self.num == 0:
+                        self.image = image1_go_down
+                    elif self.num == 1:
+                        self.image = image2_go_down
+                    else:
+                        self.image = image3_go_down
+                if self.slow:
+                    self.v_y = 25
                 else:
-                    self.image = image3_go_down
-                self.v_y = 50
+                    self.v_y = 60
                 self.go_down = True
             elif args[0].type == pygame.KEYUP and args[0].key == pygame.K_DOWN and self.go_down:
                 self.v_y = 0
                 self.go_down = False
 
             if args[0].type == pygame.KEYDOWN and args[0].key == pygame.K_UP and not self.go_up:
-                if self.num == 0:
-                    self.image = image1_go_up
-                elif self.num == 1:
-                    self.image = image2_go_up
+                if self.level != 4:
+                    if self.num == 0:
+                        self.image = image1_go_up
+                    elif self.num == 1:
+                        self.image = image2_go_up
+                    else:
+                        self.image = image3_go_up
+                if self.slow:
+                    self.v_y = -25
                 else:
-                    self.image = image3_go_up
-                self.v_y = -70
+                    self.v_y = -60
                 self.go_up = True
             elif args[0].type == pygame.KEYUP and args[0].key == pygame.K_UP and self.go_up:
                 self.v_y = 0
@@ -91,7 +94,10 @@ class Creature(pygame.sprite.Sprite):
                     self.image = image2_go_left
                 else:
                     self.image = image3_go_left
-                self.v_x = -70
+                if self.slow:
+                    self.v_x = -25
+                else:
+                    self.v_x = -60
                 self.go_left = True
             elif args[0].type == pygame.KEYUP and args[0].key == pygame.K_LEFT and self.go_left:
                 self.v_x = 0
@@ -104,13 +110,16 @@ class Creature(pygame.sprite.Sprite):
                     self.image = image2_go_right
                 else:
                     self.image = image3_go_right
-                self.v_x = 50
+                if self.slow:
+                    self.v_x = 25
+                else:
+                    self.v_x = 60
                 self.go_right = True
             elif args[0].type == pygame.KEYUP and args[0].key == pygame.K_RIGHT and self.go_right:
                 self.v_x = 0
                 self.go_right = False
-            self.rect.y += self.v_y * self.clock_y.tick() / 1000
-            self.rect.x += self.v_x * self.clock_x.tick() / 1000
+            self.rect.y += self.v_y * self.clock_y.tick() / 100
+            self.rect.x += self.v_x * self.clock_x.tick() / 100
             self.mask = pygame.mask.from_surface(self.image)
             # print('================', self.rect.x, self.rect.y, self.v_x, self.v_y)
         else:
