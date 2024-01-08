d28c742fae67a09dead8de5d7a2351ff557bec1a
kirilllllllllllllllllllllllllllll <150334314+kirilllllllllllllllllllllllllllll@users.noreply.github.com>
2024-01-08 12:54:16 +0700

    Add files via upload
    commit: level_4, работа с текстовым файлом

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from creature import Creature
 from level_1 import level_with_bombs
 from level_2 import level_with_little_bad_fish
 from level_3 import level_underwater_castle
+from level_4 import level_cave
 
 # Изображение не получится загрузить
 # без предварительной инициализации pygame
@@ -22,7 +23,7 @@ vertical_borders = pygame.sprite.Group()
 all_sprites = pygame.sprite.Group()
 
 if __name__ == '__main__':
-    score = 0
+    results = []
     pygame.init()
     size = width, height = 800, 800
     screen = pygame.display.set_mode(size)
@@ -49,9 +50,9 @@ if __name__ == '__main__':
     screen.blit(image1_go_right, (75, 125))
     screen.blit(image2_go_right, (316, 125))
     screen.blit(image3_go_right, (557, 125))
-    pygame.draw.rect(screen, (255, 255, 255), (75, 330, 166, 100), width=2)
-    pygame.draw.rect(screen, (255, 255, 255), (316, 330, 166, 100), width=2)
-    pygame.draw.rect(screen, (255, 255, 255), (557, 330, 166, 100), width=2)
+    pygame.draw.rect(screen, (255, 255, 255), (75, 330, 166, 100), width=0)
+    pygame.draw.rect(screen, (255, 255, 255), (316, 330, 166, 100), width=0)
+    pygame.draw.rect(screen, (255, 255, 255), (557, 330, 166, 100), width=0)
     text(screen, 'Выберите персонажа:', (50, 10))
     text(screen, 'Выбрать', (80, 360))
     text(screen, 'Выбрать', (326, 360))
@@ -78,29 +79,29 @@ if __name__ == '__main__':
 
     # если не закрыли окно
     if not stop_all:
-        '''start_screen(screen, ["ЗАСТАВКА", "",
-                              "Правила игры",
-                              "Если в правилах несколько строк,",
-                              "приходится выводить их построчно"], 'ship.jpg', (800, 800))
-
-        # следующий уровень
-        start = False
-        death = False
-        level_with_bombs(screen, num)
-        # level_underwater_castle()
-        start_screen(screen, ["ЗАСТАВКА", "",
-                              "Правила игры",
-                              "Если в правилах несколько строк,",
-                              "приходится выводить их построчно"], 'fon.jpg', (1364, 800))
-        level_with_little_bad_fish(screen, num)'''
-
+        start_screen(screen, ["ПОДВОДНАЯ ПЕЩЕРА", "",
+                              "Пещера не представляет особой опасности.",
+                              "Чего тебе и стоит опасаться, так это водорослей, растущих в пещере",
+                              "Наткнёшься на них - и уже не выпутаешься",
+                              "Так что будь осторожен",
+                              "Течение в пещере такое сильное, что бороться с ним нет смысла,",
+                              "так что двигаться вперёд-назад у тебя не получится."], 'cave2.jpg', (800, 800), True)
+        num_of_death = level_cave(screen, num)
+        results.append(num_of_death)
+        start_screen(screen, ["ПОДВОДНАЯ ПЕЩЕРА", "",
+                              "Что ж, пещера пройдена,",
+                              "но всё самое интересное только впереди", ''
+                                                                        'Количество попыток: ' + str(num_of_death + 1)],
+                     'cave2.jpg', (800, 800), True)
         start_screen(screen, ["ПОДВОДНЫЙ ЗАМОК", "",
                               "Никто точно не знает, как этот замок оказался под водой",
                               "однако уже много лет здесь проживает особая банда рыб.",
                               "Если честно, умом они не блещут, да и зрение у них не самое лучшее.",
                               "Чтобы избежать неприятностей, тебе нужно не попадаться всего 40 секунд,",
                               "затем путь дальше будет свободен"], 'castle.jpg', (800, 800))
+
         num_of_death = level_underwater_castle(screen, num)
+        results.append(num_of_death)
         start_screen(screen, ["ПОДВОДНЫЙ ЗАМОК", "",
                               "Наконец ты отделался от этой банды",
                               "Правда впереди у тебя ещё будут подобные неприятные встречи...",
@@ -119,13 +120,14 @@ if __name__ == '__main__':
                               '(хотя у рыбы с арифметикой вообще-то не должно быть проблем)',
                               'в левом верхнем углу будет счётчик бомб'], 'ship.jpg', (800, 800))
         num_of_death = level_with_bombs(screen, num)
+        results.append(num_of_death)
         start_screen(screen, ["ЗАТОНУВШИЙ КОРАБЛЬ", "",
                               "Надеюсь, ты научился кое-каким приёмам при прохождении",
                               "этого уровня, так как они тебе очень понадобятся при прохождении",
                               'следующего уровня, где ты вновь встретишься с бандой рыб,',
                               'которые на самом деле не намного умнее этих бомб, но об этом позже', ''
-                                                                                      'Количество попыток: ' + str(
-                num_of_death + 1)], 'ship.jpg', (800, 800))
+                              'Количество попыток: ' + str(
+                               num_of_death + 1)], 'ship.jpg', (800, 800))
 
         start_screen(screen, ["СРЕДИЗЕМНОМОРСКИЕ РАЗБОЙНИКИ", "",
                               "Пожалуй, одна из самых надоедливых банд.",
@@ -135,8 +137,17 @@ if __name__ == '__main__':
                               "Остаётся только пожелать тебе удачи, поскольку она тебе пригодится)"], 'fon.jpg',
                      (1364, 800))
         level_with_little_bad_fish(screen, num)
+        results.append(num_of_death)
         start_screen(screen, ["СРЕДИЗЕМНОМОРСКИЕ РАЗБОЙНИКИ", "",
                               "Удивлён, но ты всё же смог справиться с этим уровнем. Поздравляю!",
                               "",
                               'Количество попыток: ' + str(num_of_death + 1)], 'fon.jpg',
                      (1364, 800))
+        with open('results.txt', 'w') as file:
+            for i in range(len(results)):
+                file.write(str(i + 1) + ' уровень. Количество попыток: ' + str(results[i] + 1) + '\n')
+        with open('results.txt', 'r') as file:
+            data = file.readlines()
+            data = ['РЕЗУЛЬТАТЫ'] + data
+            start_screen(screen, data, 'cave2.jpg', (800, 800), True)
+
